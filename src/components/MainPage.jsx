b3b04d6d7e8758c9a4417660d026bb91a35707f2
lesley_w <lrwesley92@gmail.com>
2024-01-04 19:01:42 -0600

    Refactored code so main page is its own component

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../components/Header.jsx"
+
+function MainPage() {
+  return (
+    <div className="main-page">
+        <Header />
+    </div>
+  )
+}
+
+export default MainPage
