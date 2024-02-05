51a1e75eb1a7f08afec63522a7bda12483bbbcd1
lesley_w <lrwesley92@gmail.com>
2024-02-04 22:48:22 -0600

    Added conditional stylig for Reward Options if option is sold out

--- src/components/RewardOption.jsx
+++ src/components/RewardOption.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function RewardOptionAvailable(props) {
   return (
-    <div className="reward-option">
+    <div className={props.soldOut ? "reward-option sold-out" : "reward-option"}>
         <div className="option-heading">
             <h3>{props.name}</h3>
             <span className="option-description">Pledge {props.pledgeAmount} or more</span>
