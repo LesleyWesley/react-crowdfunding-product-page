b87aa62b11dd97a3b3e83862f06a5c3d96388580
lesley_w <lrwesley92@gmail.com>
2024-02-06 23:19:38 -0600

    Added file for SelectionModal component and connected it to App component. Also disabled Select Reward buttons if soldOut state is true

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MainPage from "./components/MainPage.jsx"
+import SelectionModal from "./components/SelectionModal.jsx"
 import data from "./data.json"
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
   return (
     <div className="app">
       <MainPage data={data} bookmarked={bookmarked} handleClick={toggleBookmark}/>
+      <SelectionModal />
     </div>
   )
 }
---
+++ src/components/SelectionModal.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function SelectionModal(){
+  return (
+    <h1>This is the Selection Modal!</h1>
+  )
+}
+
+export default SelectionModal
