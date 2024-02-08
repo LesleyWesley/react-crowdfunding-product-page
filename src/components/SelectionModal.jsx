da232aa39fc92304c7f6d3e5da65a310d8c57275
lesley_w <lrwesley92@gmail.com>
2024-02-08 13:16:46 -0600

    Added the header content of the selection modal

--- src/components/SelectionModal.jsx
+++ src/components/SelectionModal.jsx
@@ -4,6 +4,11 @@ function SelectionModal(){
   return (
     <div className="selection-modal-bg">
       <div className="selection-modal-container">
+          <div className="modal-header">
+              <h2>Back this project</h2>
+              <img src="icon-close-modal.svg" alt="close modal" />
+          </div>
+          <p className="modal-description">Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?</p>
       </div>
     </div>
   )
