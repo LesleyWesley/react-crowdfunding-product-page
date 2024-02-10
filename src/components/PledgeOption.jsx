cf820a7893cbf38927c30667804e2eb9c6e1ddc1
lesley_w <lrwesley92@gmail.com>
2024-02-09 18:42:41 -0600

    Added jsx for PledgeOption component

---
+++ src/components/PledgeOption.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+function PledgeOption() {
+  return (
+    <div className="pledge-option-container">
+        <div className="pledge-header">
+            <div className="selected-container">
+                <div className="selected-fill"></div>
+            </div>
+            <div className="pledge-header-details">
+                <h3>Bamboo Stand</h3>
+                <span className="option-description">Pledge $25 or more</span>
+            </div>
+        </div>
+        <p>You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campain, and you'll be added to a special Backer member list.</p>
+        <p>
+          <span className="amount-remaining">101</span>
+          left
+        </p>
+    </div>
+  )
+}
+
+export default PledgeOption
--- src/components/SelectionModal.jsx
+++ src/components/SelectionModal.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import PledgeOption from "./PledgeOption.jsx"
 
 function SelectionModal(){
   return (
@@ -9,6 +10,7 @@ function SelectionModal(){
               <img src="icon-close-modal.svg" alt="close modal" />
           </div>
           <p className="modal-description">Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?</p>
+          <PledgeOption />
       </div>
     </div>
   )
