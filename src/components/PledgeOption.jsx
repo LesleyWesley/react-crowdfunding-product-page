eaeb180e3884b1a593a0e2b78475b7719853d144
lesley_w <lrwesley92@gmail.com>
2024-02-17 20:37:52 -0600

    Changed PledgeOption components to radio inputs

--- src/components/PledgeOption.jsx
+++ src/components/PledgeOption.jsx
@@ -3,32 +3,35 @@ import React from "react"
 function PledgeOption(props) {
 
   const [expanded, setExpanded] = React.useState(false)
-  
+
 
   return (
-    <div className={props.soldOut ? "pledge-option-container sold-out" : "pledge-option-container"}>
-        <div className="pledge-header">
-            <div className="selected-container">
-                <div className="selected-fill"></div>
+    <label>
+        <input type="radio" name="pledgeOption" />
+        <div className={props.soldOut ? "pledge-option-container sold-out" : "pledge-option-container"}>
+            <div className="pledge-header">
+                <div className="selected-container">
+                    <div className="selected-fill"></div>
+                </div>
+                <div className="pledge-header-details">
+                    <h3>{props.name}</h3>
+                    {props.pledgeAmount && <span className="option-description">Pledge {props.pledgeAmount} or more</span>}
+                </div>
             </div>
-            <div className="pledge-header-details">
-                <h3>{props.name}</h3>
-                {props.pledgeAmount && <span className="option-description">Pledge {props.pledgeAmount} or more</span>}
-            </div>
+            <p>{props.description}</p>
+            {props.pledgeAmount && <p className="remaining-pledges-section">
+              <span>{props.remaining}</span>
+              left
+            </p>}
+            <form>
+                <h4>Enter your pledge</h4>
+                <div classname="form-elements">
+                    <input type="number" />
+                    <button>Continue</button>
+                </div>
+            </form>
         </div>
-        <p>{props.description}</p>
-        {props.pledgeAmount && <p className="remaining-pledges-section">
-          <span>{props.remaining}</span>
-          left
-        </p>}
-        <form>
-            <h4>Enter your pledge</h4>
-            <div classname="form-elements">
-                <input type="number" />
-                <button>Continue</button>
-            </div>
-        </form>
-    </div>
+    </label>
   )
 }
 
